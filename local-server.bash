ed4fb554dde6fc72117e3cc1b5b3cf0b6d9fabfd
Alma Eyre <aselunar@gmail.com>
2025-12-01 20:40:11 -0600

    local server python

--- local-server.bash
+++ local-server.bash
@@ -1,10 +1,2 @@
-#!/bin/sh
-
-# 1. Download: Save to a file so Python can read from keyboard (stdin)
-curl -sL "[http://host.docker.internal:8000/setup.py](http://host.docker.internal:8000/setup.py)" -o setup.py
-
-# 2. Run: '< /dev/tty' forces Python to read from your keyboard, not the curl pipe
-python3 setup.py < /dev/tty
-
-# 3. Cleanup: Delete the file to leave no trace
-rm setup.py
+# Run this in the root of your Burndown-Agent folder
+python3 -m http.server 8000
