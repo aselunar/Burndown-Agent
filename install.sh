269cf1cf4a449e2b7b5e8a84b1fe573d9d597ea7
Alma Eyre <aselunar@gmail.com>
2025-12-01 20:43:18 -0600

    install file

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+
+# 1. Download: Save to a file so Python can read from keyboard (stdin)
+
+curl -sL "[http://host.docker.internal:8000/setup.py](http://host.docker.internal:8000/setup.py)" -o setup.py
+
+
+
+# 2. Run: '< /dev/tty' forces Python to read from your keyboard, not the curl pipe
+
+python3 setup.py < /dev/tty
+
+
+
+# 3. Cleanup: Delete the file to leave no trace
+
+rm setup.py
