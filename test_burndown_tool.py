8534263d6a7912881cdff237efdf41627b5a55c5
Alma Eyre <aselunar@gmail.com>
2025-12-04 03:10:38 -0600

    test explanatory comment

--- test_burndown_tool.py
+++ test_burndown_tool.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.insert(0, '.')  # Or sys.path.insert(0, '.roo') if testing installed version
 
+# Can only test once env variables are set. To setup use `python3 setup.py`
 from burndown_server import _get_burndown_tasks_impl
 
 print("=== Testing without prioritize_parents ===")
